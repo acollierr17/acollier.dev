e29cc055f0e1cf2477ef0541850486cd0848e5e8
Anthony Collier <hello@acollier.dev>
2021-11-15 02:58:29 -0500

    :fire: Remove unused layout

--- layouts/post.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Heading } from '@chakra-ui/react';
-
-export default function PostLayout({ children, post }) {
-  return (
-    <Container maxW={'7xl'} p="12">
-      <Heading as="h1">{post.title}</Heading>
-    </Container>
-  );
-}
