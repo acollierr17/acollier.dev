5e04bbd6b5b0cfb9107b36adba430eeaa08a3395
Anthony Collier <hello@acollier.dev>
2021-11-16 00:51:05 -0500

    :fire: Remove dummy route

--- pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ name: 'John Doe' });
-}
