4a306c471f0bef5df1944ff9985b5dff276393ca
Anthony Collier <hello@acollier.dev>
2021-11-16 01:03:57 -0500

    :sparkles: Implement aggregate blog post view count

--- pages/api/views/index.ts
+++ pages/api/views/index.ts
@@ -1,8 +1,20 @@
+// Credit: https://github.com/leerob/leerob.io/blob/main/pages/api/views/index.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@lib/prisma';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  return res.status(200).json({ message: 'Got it boss!' });
+  try {
+    const totalViews = await prisma.views.aggregate({
+      _sum: {
+        count: true,
+      },
+    });
+
+    return res.status(200).json({ total: totalViews!._sum.count!.toString() });
+  } catch (e: any) {
+    return res.status(500).json({ message: e.message });
+  }
 }
