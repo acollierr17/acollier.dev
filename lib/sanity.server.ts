36128809b2ab5f7a4a06960581fbc2d0f4a84ef3
Anthony Collier <hello@acollier.dev>
2021-11-17 00:49:08 -0500

    :construction: Begin work on generating a sitemap

--- lib/sanity.server.ts
+++ lib/sanity.server.ts
@@ -3,7 +3,7 @@ import type { PreviewData } from 'next';
 import { createClient } from 'next-sanity';
 
 import { sanityConfig } from './config';
-import { indexQuery } from './queries';
+import { indexQuery, postSlugsQuery } from './queries';
 
 if (!sanityConfig.projectId) {
   throw Error('The Project ID is not set. Check your environment variables.');
@@ -17,5 +17,6 @@ export const getClient = (usePreview?: PreviewData) =>
   usePreview ? previewClient : sanityClient;
 
 export const getAllBlogPosts = () => sanityClient.fetch(indexQuery);
+export const getAllBlogPostSlugs = () => sanityClient.fetch(postSlugsQuery);
 
 export default sanityClient;
