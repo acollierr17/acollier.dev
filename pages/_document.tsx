7b6ca2d449725f3153ffcaddc14707bf8b53227e
Anthony Collier <hello@acollier.dev>
2021-11-21 19:16:34 -0500

    :art: Add head tags for favicon support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,35 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#dd9323"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Anthony Collier" />
+        <meta name="application-name" content="Anthony Collier" />
+        <meta name="msapplication-TileColor" content="#dd9323" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#dd9323" />
+        <link
           href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,400;0,500;0,700;1,800&display=swap"
           rel="stylesheet"
         />
