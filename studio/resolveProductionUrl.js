146e9adc41195550ef4c5d696e9aacf44bce7f33
Anthony Collier <hello@acollier.dev>
2021-11-22 02:07:18 -0500

    :art: Update environment variable

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -8,10 +8,7 @@ export default function resolveProductionUrl(doc) {
   const previewUrl = new URL(baseUrl);
 
   previewUrl.pathname = '/api/preview';
-  previewUrl.searchParams.append(
-    'secret',
-    process.env.SANITY_STUDIO_PREVIEW_SECRET,
-  );
+  previewUrl.searchParams.append('secret', process.env.SANITY_PREVIEW_SECRET);
   previewUrl.searchParams.append('slug', doc?.slug?.current ?? '/');
 
   return previewUrl.toString();
