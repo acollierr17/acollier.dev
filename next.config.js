7b7ffb2ed5977472910772d7c8130ddbdac08d20
Anthony Collier <hello@acollier.dev>
2021-11-23 01:46:24 -0500

    :heavy_plus_sign: Add preact

--- next.config.js
+++ next.config.js
@@ -25,6 +25,18 @@ const nextConfig = {
     runtimeCaching,
     buildExcludes: [/middleware-manifest.json$/],
   },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = withPWA(nextConfig);
